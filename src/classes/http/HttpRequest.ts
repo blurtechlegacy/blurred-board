e9498dab0b6bff64b40c2a182f83851376d6e687
batyshkaLenin <auroragamesproject@gmail.com>
2020-03-09 13:38:41 +0700

    Added data display

--- src/classes/http/HttpRequest.ts
+++ src/classes/http/HttpRequest.ts
@@ -1,36 +1,28 @@
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios'
 import Logger from 'src/classes/utils/Logger'
-import { HttpCancel } from 'src/classes/http/HttpCancel'
 
 class HttpRequest {
-  private headers: IObjectAny
   private readonly client: AxiosInstance
 
-  constructor(header: Object) {
-    this.headers = header
+  constructor() {
     this.client = axios.create()
   }
 
-  public async get(url: string, cancelObject?: HttpCancel) {
+  public async get(url: string) {
     const options: AxiosRequestConfig = {
       url,
       method: 'GET',
-      headers: this.headers,
+      headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Access-Control-Allow-Origin': '*',
+      },
       params: {},
       data: undefined,
-      cancelToken: new axios.CancelToken(c => {
-        if (cancelObject) {
-          cancelObject.setCancelFunction(c)
-        }
-      }),
       responseType: undefined,
     }
     try {
       return await this.client(options)
     } catch (e) {
-      if (cancelObject) {
-        cancelObject.setCancelFunction(() => {})
-      }
       Logger.warn(e)
       return {}
     }
--- src/components/Board/ServiceCell/ServiceCell.tsx
+++ src/components/Board/ServiceCell/ServiceCell.tsx
@@ -3,6 +3,7 @@ import classNames from 'classnames'
 import { IService } from 'src/classes/models/IService'
 import flag from 'src/assets/images/ico-flag.svg'
 import styles from './ServiceCell.module.scss'
+import { IStatus } from 'src/classes/models/IStatus'
 
 interface IProps {
   className: string
@@ -10,19 +11,21 @@ interface IProps {
 }
 
 export default function ServiceCell(props: IProps) {
-  const service: IService = props.service
+  const { service } = props
 
+  console.log(service)
   return (
     <div className={classNames(props.className, styles.service_cell)}>
       <div className={styles.top_info}>
         <span className={styles.service_status}>
-          STATUS: <b>{service.Status}</b>
+          STATUS: <b>{IStatus[service.status]}</b>
         </span>
         <span className={styles.service_sla}>SLA: {service.SLA}%</span>
+        <span className={styles.service_fp}>FP: {service.fp}</span>
       </div>
       <div className={styles.bottom_info}>
         <img src={flag} alt={'flag'} className={styles.service_flags} />
-        <span>{service.Flags}</span>
+        <span>{`${service.flags}/${service.sflags}`}</span>
       </div>
     </div>
   )
