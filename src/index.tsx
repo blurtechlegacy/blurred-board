9fd20f4ea0417124f944775901a1cc2170ffbab7
batyshkaLenin <auroragamesproject@gmail.com>
2020-03-12 00:10:53 +0700

    Added ErrorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,20 @@ import 'src/index.module.scss'
 import { Main } from 'src/components/Main'
 import { initStore } from './store'
 import { Provider } from 'react-redux'
+import * as Sentry from '@sentry/browser'
+import ErrorBoundary from 'src/components/shared/ErrorBoundary'
 
 const store = initStore()
+process.env.REACT_APP_SENTRY &&
+  Sentry.init({ dsn: process.env.REACT_APP_SENTRY })
 
 const Root = () => {
   return (
-    <>
+    <ErrorBoundary>
       <Provider store={store}>
         <Main />
       </Provider>
-    </>
+    </ErrorBoundary>
   )
 }
 
