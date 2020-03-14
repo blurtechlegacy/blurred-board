0f0e2574ec9eaab7fc4a0e34fe81541a28807521
batyshkaLenin <auroragamesproject@gmail.com>
2020-03-14 12:36:27 +0700

    Refactor api and mapping

--- src/classes/models/ICurrent.ts
+++ src/classes/models/ICurrent.ts
@@ -30,39 +30,44 @@ export interface ICurrentRaw {
   services: IServiceRaw[]
 }
 
+const servicesMap = (item: ICommandRaw, services: IServiceRaw[]) =>
+  item.services.map((service: IService, index: number) => {
+    const old = item.old_services[index]
+    return {
+      id: service.id,
+      name: services[index].name,
+      active: services[index].active,
+      disableInterval: services[index].disable_interval,
+      flags: service.flags,
+      oldFlags: old.flags,
+      fp: service.fp,
+      oldFp: old.fp,
+      sflags: service.sflags,
+      oldSflags: old.sflags,
+      sla: service.sla,
+      oldSla: old.sla,
+      status: service.status,
+      stdout: service.stdout,
+    }
+  })
+
+const scoreboardMap = (scoreboard: ICommandRaw[], services: IServiceRaw[]) =>
+  scoreboard.map((item: ICommandRaw) => {
+    return {
+      id: item.team_id,
+      name: item.name,
+      score: item.score,
+      oldScore: item.old_score,
+      round: item.round,
+      place: item.d,
+      bias: item.n,
+      services: servicesMap(item, services),
+    }
+  })
+
 export const rawCastCurrent = (data: ICurrentRaw): ICurrent => {
   return {
     round: data.round,
-    scoreboard: data.scoreboard.map((item: ICommandRaw) => {
-      return {
-        id: item.team_id,
-        name: item.name,
-        score: item.score,
-        oldScore: item.old_score,
-        round: item.round,
-        place: item.d,
-        bias: item.n,
-        services: item.services.map((service: IService, index: number) => {
-          const services = Object.values(data.services)[index]
-          const old = item.old_services[index]
-          return {
-            id: service.id,
-            name: services.name,
-            active: services.active,
-            disableInterval: services.disable_interval,
-            flags: service.flags,
-            oldFlags: old.flags,
-            fp: service.fp,
-            oldFp: old.fp,
-            sflags: service.sflags,
-            oldSflags: old.sflags,
-            sla: service.sla,
-            oldSla: old.sla,
-            status: service.status,
-            stdout: service.stdout,
-          }
-        }),
-      }
-    }),
+    scoreboard: scoreboardMap(data.scoreboard, Object.values(data.services)),
   }
 }
--- src/classes/services/BoardStoreService.ts
+++ src/classes/services/BoardStoreService.ts
@@ -2,6 +2,8 @@ import * as BoardApi from 'src/classes/services/api/BoardApi'
 import { ICommandData } from 'src/classes/models/IInfo'
 import { IService } from 'src/classes/models/IHistory'
 import { getState, setNextState } from 'src/store'
+import { IState } from '../../store/state'
+import { ICurrent } from '../models/ICurrent'
 
 export const init = async () => {
   const state = getState()
@@ -21,6 +23,27 @@ export const init = async () => {
   setInterval(getBoard, 300000)
 }
 
+const commandsMap = (state: IState, current: ICurrent) =>
+  state.info.commands
+    .map((team: ICommandData) => {
+      const cur = current.scoreboard.find((i: ICommandData) => i.id === team.id)
+      return { ...team, ...cur }
+    })
+    .map((team: ICommandData) => {
+      const newServices = team.services.map(
+        (service: IService, index: number) => {
+          return {
+            ...service,
+            name: state.info.services[index],
+          }
+        }
+      )
+      return {
+        ...team,
+        services: newServices,
+      }
+    })
+
 export const getBoard = () => {
   const state = getState()
   const PFirstblood = BoardApi.fetchFirstblood()
@@ -28,28 +51,7 @@ export const getBoard = () => {
   Promise.all([PFirstblood, PHistory]).then(all => {
     const firstblood = all[0]
     const history = all[1]
-    const current = state.current
-    const teams = state.info.commands
-      .map((team: ICommandData) => {
-        const cur = current.scoreboard.find(
-          (i: ICommandData) => i.id === team.id
-        )
-        return { ...team, ...cur }
-      })
-      .map((team: ICommandData) => {
-        const newServices = team.services.map(
-          (service: IService, index: number) => {
-            return {
-              ...service,
-              name: state.info.services[index],
-            }
-          }
-        )
-        return {
-          ...team,
-          services: newServices,
-        }
-      })
+    const teams = commandsMap(state, state.current)
     setNextState({
       ...state,
       statuses: {
